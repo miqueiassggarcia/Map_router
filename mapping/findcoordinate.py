e851c740c39cf79988031142b4d0a82f8d78c8b7
Miquéias Garcia <mglucenalucena@gmail.com>
2024-04-20 22:36:02 -0300

    refazendo função de encontrar rua

--- mapping/findcoordinate.py
+++ mapping/findcoordinate.py
@@ -7,22 +7,22 @@ with open('data/patos.geojson') as f:
 # Extract nodes and their coordinates
 Roads = []
 for feature in data['features']:
-  if(feature['geometry']['type'] == 'LineString'):
-  name = feature['properties']['name']
-  print(name)
-  if("Lagoa do Loredo" in name):
-    Roads.append({
-      "type": feature["type"],
-      "properties": {
-        "@id": feature['properties']['@id'],
-        "name": name,
-        "highway": feature['properties']['highway']
-      },
-      "geometry": {
-        "type": feature["geometry"]["type"],
-        "coordinates": feature['geometry']['coordinates']
-      }
-    })
-    
+  if('name' in feature['properties']):
+    name = feature['properties']['name']
+    if("Rua Titico Gomes" in name):
+      Roads.append({
+        "type": feature["type"],
+        "properties": {
+          "@id": feature['properties']['@id'],
+          "name": name,
+          "highway": feature['properties']['highway']
+        },
+        "geometry": {
+          "type": feature["geometry"]["type"],
+          "coordinates": feature['geometry']['coordinates']
+        }
+      })
 
-  
+for road in Roads:
+  Road = road["properties"]["@id"]
+  print("way(" + Road[4:] + ");")
