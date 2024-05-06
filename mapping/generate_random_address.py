effe84a3506fc80a332605aa1584f90be9a6ecc6
Miquéias Garcia <mglucenalucena@gmail.com>
2024-05-06 15:41:50 -0300

    Ajustando função de pegar coordenadas por endereço e adicionando função para gerar endereços aleatórios

---
+++ mapping/generate_random_address.py
@@ -0,0 +1,9 @@
+import json
+import random
+
+with open("data/ceps.json", "r", encoding="utf-8") as json_file:
+  ceps = json.load(json_file)
+
+def generate_random_address():
+  random_cep = random.choice(list(ceps.keys()))
+  return {"cep": random_cep, **ceps[random_cep]}
--- mapping/get_coordinates_per_address.py
+++ mapping/get_coordinates_per_address.py
@@ -1,8 +1,4 @@
-import json
 import requests
-
-with open("data/ceps.json", "r", encoding="utf-8") as json_file:
-    ceps = json.load(json_file)
 
 def get_coordenadas_por_endereco(rua, bairro):
     rua = rua.replace(" ", "%20")
@@ -28,13 +24,3 @@ def get_coordenadas_por_endereco(rua, bairro):
         print(response)
         print('Falha na solicitação. Código de status:', response.status_code)
         return None
-
-for value in ceps.values():
-    rua = value['logradouro']
-    bairro = value['bairro']
-
-    coordenadas = get_coordenadas_por_endereco("Rua Bossuet Wanderley", "Centro")
-    if coordenadas:
-        print("Coordenadas de", rua + ",", bairro + ":", coordenadas)
-    else:
-        print("Não foi possível obter as coordenadas do endereço.")
